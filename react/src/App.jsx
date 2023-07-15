9576762964483fb74e696666e03b975b4697de33
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2023-07-14 23:01:44 -0400

    wip2

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,18 +3,18 @@ import { useState } from 'react';
 const gameSize = 7;
 const disc = "◉";
 
-function Square({ value }) {
-  let colour = value === "R" ? "red" : "black";
+function Square({ value, fallingDirection }) {
   return (
     <div className="square">
-      {value ? <Disc colour={colour}/> : null}
+      {value ? <Disc colour={value} fallingDirection={fallingDirection}/> : null}
     </div>
   );
 }
 
-function Disc({ colour }) {
+function Disc({ colour, fallingDirection }) {
+  const className = colour + (fallingDirection ? ` falling-${fallingDirection}` : "");
   return (
-    <span className={colour}>{disc}</span>
+    <span className={className}>{disc}</span>
   );
 }
 
@@ -26,28 +26,38 @@ function Slot({ direction, onSlotClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i, direction) {
-    if (calculateWinner(squares) || squares[i]) {
+function Board({ redIsNext, squares, onPlay }) {
+  const nextPiece = redIsNext ? 'red' : 'black';
+
+  function handleClick(rowNum, fallingDirection) {
+    if (calculateWinner(squares)) {
       return;
     }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'R';
-    } else {
-      nextSquares[i] = 'B';
+    const row = squares[rowNum];
+    let col;
+    if (fallingDirection === 'right') {
+      col = row.findIndex(element => element === null);
     }
-    onPlay(nextSquares);
+    else {
+      // findLastIndex is a recent addition, not all browsers support
+      // it, have to roll our own instead.
+      const reverseRow = row.slice().reverse();
+      col = row.length  - reverseRow.findIndex(element => element === null) - 1;
+    }
+    
+    if(col > -1 && col < row.length) {
+      row[col] = nextPiece;
+    }
+    
+    onPlay(squares);
   }
 
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
-    let winnerColour = winner === 'R' ? 'red' : 'black';
-    status = <span>{'Winner: '}<Disc colour={winnerColour}/></span>;
+    status = <span>{'Winner: '}<Disc colour={winner}/></span>;
   } else {
-    let nextColour = xIsNext ? 'red' : 'black';
-    status = <span>{'Next player: '}<Disc colour={nextColour}/></span>;
+    status = <span>{'Next player: '}<Disc colour={nextPiece}/></span>;
   }
 
   return (
@@ -58,14 +68,13 @@ function Board({ xIsNext, squares, onPlay }) {
       [...Array(gameSize).keys()].map((row) => {
         return (
           <div key={row} className="board-row">
-            <Slot direction="🡆" onSlotClick={() => handleClick(row, "left")}/>
+            <Slot direction="🡆" onSlotClick={() => handleClick(row, "right")}/>
             {
               [...Array(gameSize).keys()].map((col) => {
-                let idx = gameSize*row + col;
-                return <Square key={col} value={squares[idx]} />;
+                return <Square key={col} value={squares[row][col]} />;
               })
             }
-            <Slot direction="🡄"  onSlotClick={() => handleClick(row, "right")}/>
+            <Slot direction="🡄"  onSlotClick={() => handleClick(row, "left")}/>
           </div>
         );
       })
@@ -76,9 +85,9 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(gameSize).fill(Array(gameSize).fill(null))]);
+  const [history, setHistory] = useState([Array(gameSize).fill(null).map(() => Array(gameSize).fill(null))]);
   const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
+  const redIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
@@ -108,7 +117,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board redIsNext={redIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -118,21 +127,5 @@ export default function Game() {
 }
 
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
   return null;
 }
